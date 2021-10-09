92285bb0763054a1e2947ded1cf10789ffe69e5b
akashasy <akashasy53@gmail.com>
2021-10-09 11:05:27 +0530

    New repo and updated all the practicals

---
+++ practicals/firebase/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+
+apiKey: "AIzaSyDoLI7K2N_0oOz1WraKO3Fc0lsFhIfrOI8",
+  authDomain: "myfirstfirepro-db945.firebaseapp.com",
+  databaseURL: "https://myfirstfirepro-db945-default-rtdb.firebaseio.com",
+  projectId: "myfirstfirepro-db945",
+  storageBucket: "myfirstfirepro-db945.appspot.com",
+  messagingSenderId: "270410513181",
+  appId: "1:270410513181:web:f3693edda18d3302e1bff4",
+};
+
+firebase.initializeApp(firebaseConfig);
