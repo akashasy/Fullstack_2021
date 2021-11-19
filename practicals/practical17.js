f76669364aa289e8cb06221aa3cdf41e92f52dd6
akashasy <akashasy53@gmail.com>
2021-11-19 20:11:06 +0530

    practical15

---
+++ practicals/practical17.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+// var uname=document.getElementById('uname');
+// var form =document.getElementById('form');
+// var email=document.getElementById('email');
+// var pass1=document.getElementById('passwrd');
+// var pass2=document.getElementById('pass2');
+
+
+// form.addEventListener('submit',(event)=> {
+//     event.preventDefault();
+//     check();
+    
+// });
+
+// function check(){
+//     var userValue=uname.value;
+//     var emailValue=email.value;
+//     var pass1Value = pass1.value;
+//     var pass2Value =  pass2.value;
+//    if(userValue ===''){
+//        setError(uname,'Username cannot be blank');
+
+//    }
+//    else{
+//        success(uname);
+
+//    }
+   
+// }
+// function setError(input,msg){
+    
+//     var formControl=input.parentElement;
+//     var span= formControl.querySelector('span');
+//     span.innerText=msg;
+//     formControl.className='forms error';
+
+// }
+
+// function success(){
+//     const formControl=input.parentElement;
+//     formControl.className='forms success';
+// }
